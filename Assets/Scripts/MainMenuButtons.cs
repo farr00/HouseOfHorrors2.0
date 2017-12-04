39b91a1033217b3df78d8f7a4df562b6d48fc425
buchanant <buchanant>
2017-12-04 13:35:23 -0500

    First changes to menu. Working

--- Assets/Scripts/MainMenuButtons.cs
+++ Assets/Scripts/MainMenuButtons.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuButtons : MonoBehaviour {
+public class MainMenuButtons : MonoBehaviour
+{
 	public Transform instructionCanvas;
 
-	public void LoadGameB (string loadGameScene) {
-		SceneManager.LoadScene(loadGameScene);
+	public void LoadGameB ()
+	{
+		SceneManager.LoadScene ("Building A F1&F2");
 	}
 
-	public void ExitGameB () {
-		Debug.Log("Quit");
-		Application.Quit();
+	public void ExitGameB ()
+	{
+		Debug.Log ("Quit");
+		Application.Quit ();
 	}
 
-	public void InstructionsB (){
-		if(instructionCanvas.gameObject.activeInHierarchy == false){
-			instructionCanvas.gameObject.SetActive(true);
+	public void InstructionsB ()
+	{
+		if (instructionCanvas.gameObject.activeInHierarchy == false) {
+			instructionCanvas.gameObject.SetActive (true);
 		} else {
-			instructionCanvas.gameObject.SetActive(false);
+			instructionCanvas.gameObject.SetActive (false);
 		}
 	}
 }
