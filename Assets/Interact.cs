677701354fcdbf15b4fb11fa6b63b05cd6a8e60b
buchanant <buchanant>
2018-01-08 12:19:55 -0500

    Almost Door, we're getting there

--- Assets/Interact.cs
+++ Assets/Interact.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Interact : MonoBehaviour
 {
+	public Vector3 com;
+	public Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		rb.centerOfMass = com;
+
+
+
+	}
+
 	void Update ()
 	{
-		
+		float y = Mathf.Clamp (rb.rotation.eulerAngles.y, -100, 270);
+		rb.rotation = Quaternion.Euler (rb.rotation.eulerAngles.x, y, rb.rotation.eulerAngles.z);
+	
 	}
 
-	void OnTriggerEnter (Collider c)
-	{
 
-
-	}
 }
 
