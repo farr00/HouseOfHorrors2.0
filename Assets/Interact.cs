75452282eb11b0edca9815fe5262c387c272cef0
buchanant <buchanant>
2018-01-08 10:54:11 -0500

    Knife??

---
+++ Assets/Interact.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+	void Update ()
+	{
+		
+	}
+
+	void OnTriggerEnter (Collider c)
+	{
+
+
+	}
+}
+
